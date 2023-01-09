cfa6a67f492b2d94a1b46916378175f3ea43a934
ibraham-vicdigit <mdrafi15429@gmail.com>
2023-01-09 18:29:59 +0600

    added move api all data

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,21 @@
 import Image from 'next/image'
 import React from 'react'
-import logo from '../public/logo.png'
+import logo from '../public/logo (3).png'
 import HeaderItem from './HeaderItem';
 export const Header = () => {
     return (
         <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
-            <div className='flex flex-grow justify-evenly max-w-2xl'>
+            <div className='lg:flex flex-grow justify-evenly max-w-2xl hidden lg:block'>
                 <HeaderItem title="HOME"  />
-                <HeaderItem title="HOME"   />
-                <HeaderItem title="HOME"  />
-                <HeaderItem title="HOME" />
-                <HeaderItem title="HOME"  />
-                <HeaderItem title="HOME"  />
+                <HeaderItem title="About"   />
+                <HeaderItem title="Contact"  />
+                <HeaderItem title="Theme" />
+                <HeaderItem title="Login"  />
+                <HeaderItem title="SingIn"  />
             </div>
             <Image src={logo}
-                width={100}
-                height={100}
+                width={130}
+                height={120}
                 className='object-contain'
                 alt='logo'
             />
--- components/HeroSection.js
+++ components/HeroSection.js
@@ -11,24 +11,24 @@ const HeroSection = () => {
                 showIndicators={false}
                 showThumbs={false}
                 interval={5000}
-                className="lg:px-8 lg:mt-8"
+                className="lg:px-8 mt-8"
             >
                 <div>
                     <img
                         className="h-65"
-                        loading="lazy" src="https://images.unsplash.com/photo-1532568547949-a09f1afe4d1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                        loading="lazy" src="https://wallpaperaccess.com/full/3075770.jpg" />
                 </div>
                 <div>
                     <img
 
                         className="h-65"
-                        loading="lazy" src="https://images.unsplash.com/photo-1510827220565-c6a086ff31c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                        loading="lazy" src="https://s.studiobinder.com/wp-content/uploads/2020/05/Best-Action-Movies-of-All-Time-Featured-.jpg" />
                 </div>
                 <div>
                     <img
                         className="h-65"
                         loading="lazy"
-                        src="https://images.unsplash.com/photo-1585647347384-2593bc35786b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                        src="https://www.whats-on-netflix.com/wp-content/uploads/2022/08/should-you-watch-carter-on-netflix.webp" />
                 </div>
             </Carousel>
         </div>
